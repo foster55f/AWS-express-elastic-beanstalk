6f48b75fe85bc45ad6accc41c0e674f134f9df82
“Foster <fosterataylor@yahoo.com>
2020-06-05 11:36:41 -0600

    Remove Hike require

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var hike = require('./routes/hike');
 
 var app = express();
 
